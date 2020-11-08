32a8644b983d8bf641c74fd880861f4410e33f33
Doraonroids <zgt4@pitt.edu>
2020-11-08 00:16:38 -0500

    added to test.c

--- test.c
+++ test.c
@@ -1,10 +1,53 @@
-#include <>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(){
+  int start =0;
+  int action =0;
+  int score =0;
+  int input =0;
+  printf("Start game?\n");
+  scanf("%d", &start);
 
-main(){
+  while(start==1){
 
-printf("hello world!/n")
-;
+    action = rand()%3;
+    
+    switch(action){
+      case 0: 
+	      printf("Case 0\n");
+              printf("Start game?\n");
+              scanf("%d",&start);
 
+	      if(start){
+	      }
+	      break;
+      
+      case 1: printf("Case 1\n"); 
+              printf("Start game?\n");
+	      scanf("%d",&start);
+
+
+	      break;
+      case 2: printf("Case 2\n");
+              printf("Start game?\n");
+	      scanf("%d", &start);
+
+
+	      break;
+      
+      case 3: printf("Case 3\n");
+              printf("Start game?\n");
+              scanf("%d", &start);
+
+
+	      break;
+
+    }
+
+    printf("Start game?\n");
+    scanf("%d",&start);
+  }
 return 0;
-
 }
+
