aaff5e89d36886a03190a824a0bcc86acf1ab197
Doraonroids <59835131+Doraonroids@users.noreply.github.com>
2020-11-10 17:00:10 -0500

    Update sketch_nov08a.ino
    added song and tone for input confirmation

--- sketch_nov08a/sketch_nov08a.ino
+++ sketch_nov08a/sketch_nov08a.ino
@@ -1,11 +1,97 @@
-
+#define NOTE_B0 31
+#define NOTE_C1 33
+#define NOTE_CS1 35
+#define NOTE_D1 37
+#define NOTE_DS1 39
+#define NOTE_E1 41
+#define NOTE_F1 44
+#define NOTE_FS1 46
+#define NOTE_G1 49
+#define NOTE_GS1 52
+#define NOTE_A1 55
+#define NOTE_AS1 58
+#define NOTE_B1 62
+#define NOTE_C2 65
+#define NOTE_CS2 69
+#define NOTE_D2 73
+#define NOTE_DS2 78
+#define NOTE_E2 82
+#define NOTE_F2 87
+#define NOTE_FS2 93
+#define NOTE_G2 98
+#define NOTE_GS2 104
+#define NOTE_A2 110
+#define NOTE_AS2 117
+#define NOTE_B2 123
+#define NOTE_C3 131
+#define NOTE_CS3 139
+#define NOTE_D3 147
+#define NOTE_DS3 156
+#define NOTE_E3 165
+#define NOTE_F3 175
+#define NOTE_FS3 185
+#define NOTE_G3 196
+#define NOTE_GS3 208
+#define NOTE_A3 220
+#define NOTE_AS3 233
+#define NOTE_B3 247
+#define NOTE_C4 262
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_DS4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_AS4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define NOTE_F5 698
+#define NOTE_FS5 740
+#define NOTE_G5 784
+#define NOTE_GS5 831
+#define NOTE_A5 880
+#define NOTE_AS5 932
+#define NOTE_B5 988
+#define NOTE_C6 1047
+#define NOTE_CS6 1109
+#define NOTE_D6 1175
+#define NOTE_DS6 1245
+#define NOTE_E6 1319
+#define NOTE_F6 1397
+#define NOTE_FS6 1480
+#define NOTE_G6 1568
+#define NOTE_GS6 1661
+#define NOTE_A6 1760
+#define NOTE_AS6 1865
+#define NOTE_B6 1976
+#define NOTE_C7 2093
+#define NOTE_CS7 2217
+#define NOTE_D7 2349
+#define NOTE_DS7 2489
+#define NOTE_E7 2637
+#define NOTE_F7 2794
+#define NOTE_FS7 2960
+#define NOTE_G7 3136
+#define NOTE_GS7 3322
+#define NOTE_A7 3520
+#define NOTE_AS7 3729
+#define NOTE_B7 3951
+#define NOTE_C8 4186
+#define NOTE_CS8 4435
+#define NOTE_D8 4699
+#define NOTE_DS8 4978 
 //define three inputs as volatile
-volatile uint8_t buttonState = 0;
+volatile uint16_t buttonState = 0;
 volatile uint16_t sliderState = 0;
 volatile uint16_t previousSliderState = 0;
 volatile uint16_t photoState = 0;
-volatile uint16_t previousPhotoState = 0;
-
 
 //define input pin assignments
 const uint8_t sliderPin = A0;
@@ -23,7 +109,6 @@ const uint8_t speakerPin = 9;
 const uint8_t voicePlayPin = 5;
 const uint8_t voicePausePin = 6;
 const uint8_t resetPin = 2;
-
 
 
 //define random number variable
@@ -66,12 +151,15 @@ volatile void buttonSequence();
 volatile void sliderSequence();
 volatile void photoSequence();
 volatile void resetSequence();
+volatile void playDarude();
 
 void setup() {
   //THIS MUST BE FIRST
   digitalWrite(resetPin, HIGH);
   delay(100);
-  
+
+
+ 
   // put your setup code here, to run once:
 
   previousTime = millis();
@@ -104,10 +192,21 @@ void setup() {
   pinMode(photoPin, INPUT);
   delay(10);
  
- 
-
+  //display usually start at 80 when first plugged in
+  //change sync variable to true to run sync
+  //change difference variable to sync properly
+  bool SYNC = false;
+  int difference = 20;
+  int i = 0;
+  while(i < 20 && SYNC){
+    digitalWrite(counterPin, HIGH);
+    delay(10);
+    digitalWrite(counterPin, LOW);
+    delay(10);
+    i++;
+  }
   randomSeed(0);
-  
+
   //play ready to play tone
   for (int thisNote = 0; thisNote < melodyNoteLength; thisNote++) {
 
@@ -184,6 +283,7 @@ void loop() {
         noTone(speakerPin);
   
       }
+      
       //blink score
       
       resetSequence();
@@ -210,27 +310,10 @@ void loop() {
     
     //play win tone
     if(score == 99){
-      for (int thisNote = 0; thisNote < wonMelodyNoteLength; thisNote++) {
-  
-        // to calculate the note duration, take one second divided by the note type.
-    
-        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc
-        int wonnoteDuration = 1000 / wonnoteDurations[thisNote];
-        tone(speakerPin, wonMelody[thisNote], wonnoteDuration);
-    
-        // to distinguish the notes, set a minimum time between them.
-        // the note's duration + 30% seems to work well:
-    
-        int wonpauseBetweenNotes = wonnoteDuration * 1.30;
-        delay(wonpauseBetweenNotes);
-        // stop the tone playing:
-        noTone(speakerPin);
-  
-      }
+      playDarude();
+      playDarude();
+      playDarude();
       //reset game
-      delay(1000);
-      digitalWrite(resetPin, LOW);
-  
       resetSequence();
     }
   }
@@ -248,6 +331,9 @@ volatile void buttonSequence(){
      flag = true;//true ends this loop and assigns new number for sequence
    
     }else if(buttonState == HIGH){//check if button was press
+      tone(speakerPin,NOTE_E4);
+      delay(100);
+      noTone(speakerPin);
       win = true;//exits with right option
       flag = true;//true ends this loop and assigns new number for sequence
     }  
@@ -266,6 +352,9 @@ volatile void sliderSequence(){
       flag = true;//true ends this loop and assigns new number for sequence
    
     }else if(sliderState != previousSliderState){//check if button was press
+       tone(speakerPin,NOTE_D4);
+      delay(100);
+      noTone(speakerPin);
       win = true;//exits with right option
       flag = true;//true ends this loop and assigns new number for sequence
     }
@@ -274,9 +363,7 @@ volatile void sliderSequence(){
 }
 
 volatile void photoSequence(){
-  
-  previousPhotoState = analogRead(photoPin) - 275 ;
- 
+  delay(30);
   while(!flag){
   
     currentTime = millis();//get current time
@@ -285,7 +372,10 @@ volatile void photoSequence(){
       win = false;//exits with time up option
       flag = true;//true ends this loop and assigns new number for sequence
    
-    }else if(previousPhotoState > photoState){//check if button was press
+    }else if(10 == photoState){//check if button was press
+       tone(speakerPin,NOTE_C4);
+      delay(100);
+      noTone(speakerPin);
       win = true;//exits with right option
       flag = true;//true ends this loop and assigns new number for sequence
    
@@ -305,8 +395,134 @@ volatile void resetSequence(){
     }
     play = false;
     score = 0;
-  //reset game
+    //reset game
     delay(1000);
     digitalWrite(resetPin, LOW);
 
+}
+
+volatile void playDarude(){
+ int shortTone = 80; //Length of Short Note
+ int longTone = 200; //Length of Long Note
+ int standardDelay = 40; //Delay
+ int delayBetweenBars = 50; //Delay
+
+  
+  for(int k = 0; k <4; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone); 
+    noTone(speakerPin); 
+    delay(standardDelay);
+  }
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+
+  
+  delay(delayBetweenBars);
+  
+  for(int k = 0; k<6; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone);
+    noTone(speakerPin);
+    delay(standardDelay); 
+  }
+  
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+  
+  for(int k = 0; k < 6; k++){
+    tone(speakerPin,  NOTE_E4, shortTone);
+    delay(shortTone);
+    noTone(speakerPin);
+    delay(standardDelay); 
+    
+  }
+  
+  tone(speakerPin,  NOTE_E4, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+ 
+  for(int k = 0; k < 6; k++){
+    tone(speakerPin,  NOTE_D4, shortTone); //Play tone, PinspeakerPin, note, length of note
+    delay(shortTone); //Length of delay
+    noTone(speakerPin); //Stop tone
+    delay(standardDelay); 
+    
+  }
+  tone(speakerPin,  NOTE_D4, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+
+  tone(speakerPin, NOTE_A3, longTone);
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+
+  
+  for(int k = 0; k <4; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone); 
+    noTone(speakerPin); 
+    delay(standardDelay);
+  }
+
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(delayBetweenBars);
+ 
+  for(int k = 0; k<6; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone);
+    noTone(speakerPin);
+    delay(standardDelay); 
+  }
+
+  
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+ 
+  tone(speakerPin, NOTE_E4, longTone);
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+
+  for(int k = 0; k <4; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone); 
+    noTone(speakerPin); 
+    delay(standardDelay);
+  }
+
+  
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(delayBetweenBars);
+ 
+  for(int k = 0; k<6; k++){
+    tone(speakerPin,  NOTE_B3, shortTone);
+    delay(shortTone);
+    noTone(speakerPin);
+    delay(standardDelay); 
+  }
+
+  
+  tone(speakerPin,  NOTE_B3, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);
+
+  tone(speakerPin, NOTE_E4, longTone); 
+  delay(longTone);
+  noTone(speakerPin);
+  delay(standardDelay);  
+  
 }
